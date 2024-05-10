8f01e7bb3008f44150a98fbf46e0c0873100c01a
Sébastien Dubé <sebastien.dube.10@ulaval.ca>
2024-05-10 12:52:31 -0400

    ajout de create relation et add document dans le argparse

--- real_life_rpg/src/server_logic/server_logic.rs
+++ real_life_rpg/src/server_logic/server_logic.rs
@@ -1,5 +1,22 @@
 use std::env;
 use crate::database_logic::database_logic::{*};
+use std::error::Error;
+use std::fmt;
+
+/*
+*@brief Custom error
+*/
+#[derive(Debug)]
+struct MyError {
+    details: String
+}
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{}",self.details)
+    }
+}
+
 /**
  * @brief This function collect information from the terminal and call serach_for_function with these information
 */
@@ -7,6 +24,38 @@ pub fn run() {
     let command: Vec<String> = env::args().collect();
     let args: Vec<String> = command[1..].to_vec();
     search_for_function(&args);
+}
+
+fn get_document(args: &Vec<String>) -> Result<DocumentType, MyError>{
+    //args[4] = document type, args[5]+ = value
+    match args[4].to_lowercase().as_str(){
+        "skill" => if args.len() >= 7{
+            let mut skill = SkillType {
+                _key: args[5].clone(),
+                name: args[6].clone(),
+            };
+            return Ok(crate::database_logic::database_logic::DocumentType::Skill(skill));
+        },
+        "user" => if args.len() >= 11{
+            match args[10].clone().parse::<i32>(){
+                Ok(_) => if true{
+                    let mut user = UserType {
+                        _key: args[5].clone(),
+                        name: args[6].clone(),
+                        pseudo: args[7].clone(),
+                        email: args[8].clone(),
+                        birth_date: args[9].clone(),
+                        level: args[10].clone().parse::<i32>().unwrap()
+                    };
+                    return Ok(crate::database_logic::database_logic::DocumentType::User(user))
+                },
+                Err(_) => return Err(MyError {details: "erreur lors de conversion string to int".to_string()}),
+                _other => print!("what the hell")
+            }
+        },
+        _other => print!("other document type, ")
+    }
+    return Err(MyError {details: "document invalide".to_string()});
 }
 
 /**
@@ -20,10 +69,17 @@ fn search_for_function(args: &Vec<String>) {
                 match args[1].to_lowercase().as_str(){
                     "database" => create_new_db(args[2].clone()),
                     "collection" => if args.len() >= 4{
-                        create_new_collection(args[2].clone(), args[3].clone())
+                        create_new_collection(args[2].clone(), args[3].clone())//collection name, database name
                     },
-                    "document" =>  print!("add document {}", args[2].as_str()),
-                    "relation" => print!("add relation {}", args[2].as_str()),
+                    "document" =>  if args.len() >= 5{ //collection name, database name, document type, value...
+                        match get_document(&args){
+                            Ok(doc) => add_document_to_collection(doc, args[2].clone(), args[3].clone()),
+                            Err(e) => print!("{}", e)
+                        }
+                    },
+                    "relation" => if args.len() >= 4{
+                        create_new_relation(args[2].clone(),args[3].clone()) // relation name, database name
+                    },
                     _other => print!("add other")
                 }
             },
