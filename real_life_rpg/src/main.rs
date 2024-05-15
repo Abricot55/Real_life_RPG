1372fb145548773a52589facca82c20476eab1d4
Sébastien Dubé <sebastien.dube.10@ulaval.ca>
2024-05-15 17:46:07 -0400

    début d'un serveur

--- real_life_rpg/src/main.rs
+++ real_life_rpg/src/main.rs
@@ -2,10 +2,24 @@ mod server_logic;
 mod database_logic;
 use crate::server_logic::server_logic::*;
 use std::error::Error;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main()  -> Result<(), Box<dyn Error>> {
-    run().await;
+    let what_to_do: i32 = 1;
+    if what_to_do == 0{
+        //server
+        let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+        for stream in listener.incoming() {
+            let stream = stream.unwrap();
+
+            println!("Connection established!");
+        }
+    } else if what_to_do == 1{
+        //parser
+        run().await;
+    }
     Ok(())
 }
 
