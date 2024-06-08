2dc996d831bc38b107fd3d3d17c2e4486117eba2
Adam Côté <148469353+Abricot55@users.noreply.github.com>
2024-06-08 18:06:28 -0400

    smthing dont work anymore

--- real_life_rpg/src/util.rs
+++ real_life_rpg/src/util.rs
@@ -1,8 +1,12 @@
 use regex::Regex;
-
+use bcrypt::{hash, DEFAULT_COST};
 
 pub fn is_valid_email(email: &str) -> bool {
     let pattern = r"^[\w.+-]+@\w+\.\w{2,}$";
     let regex = Regex::new(pattern).unwrap();
     regex.is_match(email)
+}
+
+pub fn hash_string(base_string : &str) -> String{
+    return hash(base_string, DEFAULT_COST).unwrap();
 }
