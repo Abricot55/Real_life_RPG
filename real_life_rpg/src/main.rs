98ee937ebb770807f9e4e166b7f4aff952ee264f
Adam Côté <148469353+Abricot55@users.noreply.github.com>
2024-08-14 17:30:24 -0400

    we'll do anti ddos later

--- real_life_rpg/src/main.rs
+++ real_life_rpg/src/main.rs
@@ -1,18 +1,14 @@
 mod database_logic;
 mod server_logic;
 mod util;
-use std::time::Duration;
 
 use crate::server_logic::friend_server_request::*;
 use crate::server_logic::message_server_request::*;
 use crate::server_logic::photo_server_request::*;
 use crate::server_logic::user_server_request::*;
-use std::sync::Arc;
-use tower_governor::governor::GovernorConfigBuilder;
-use tower_governor::key_extractor::SmartIpKeyExtractor;
+
+
 use warp::Filter;
-
-
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -25,11 +21,12 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         Err(_) => print!("AS")
     }*/
 
-    let routes = friend_routes()
-        .or(photo_routes())
-        .or(user_routes())
-        .or(message_routes());
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3000)).await;
+    let routes = friend_routes()
+    .or(photo_routes())
+    .or(user_routes()).or(message_routes());
+
+        warp::serve(routes.clone()).run(([127, 0, 0, 1], 3000)).await; 
+  
     Ok(())
 }
